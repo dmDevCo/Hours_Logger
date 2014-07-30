fe724eadadc447d4f79de97f614476fc6ba8be01
Drew Haines <drewhaines@yahoo.com>
2014-07-29 21:54:47 -0700

    First Commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HoursLogger::Application.config.secret_key_base = '56a7e53cb0b1fd101a0b95c11eb2cbb0ed000d409d9103e5e241d657e1072870e1fa784f1ef59a1d73266a34364b7562fbc1d88e1aaeb0f367633b1a28894ba3'
